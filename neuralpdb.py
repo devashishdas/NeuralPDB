927807e511ce661ce39b9b5675794cb3454dffa2
Devashish Das <dasdevashishdas@gmail.com>
2023-09-04 09:25:26 +0000

    a1

--- neuralpdb.py
+++ neuralpdb.py
@@ -1,13 +1,13 @@
+import sys
 def ErrorCheck(func):
-    """
-    """
-    def wrapper(arg):
+    def wrapper(pp, arg):
         if arg != arg.strip():
             print("[WARN]: space in PDBID")
         if len(arg) != 4:
             print("[FError]: Cannot continue. PDBID not valid.")
             raise ValueError
-        func(arg)
+        func(pp, arg)
+        return wrapper
 
 
 class Protein:
@@ -17,3 +17,7 @@ class Protein:
     def __init__(self, Pdb) -> None:
         self.pdbid = Pdb
         self.url = "https://files.rcsb.org/view/{self.pdbid}.pdb"
+
+
+p = Protein(sys.argv[-1])
+print(p.url)
