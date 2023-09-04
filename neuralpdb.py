31f1e904a507c54677aa479a1f1a8094482b74b9
Devashish Das <dasdevashishdas@gmail.com>
2023-09-04 09:40:54 +0000

    c

--- neuralpdb.py
+++ neuralpdb.py
@@ -1,23 +1,42 @@
 import sys
+import argparse
+
+argparser = argparse.ArgumentParser(description="Get PDBID")
+argparser.add_argument("-p", "--PDBID", help="PDBID of the protein")
+args = argparser.parse_args()
+
+
+# Decorator to check if the PDBID is valid
+
+
+
 def ErrorCheck(func):
-    def wrapper(pp, arg):
+    def wrapper(ppx, arg):
         if arg != arg.strip():
             print("[WARN]: space in PDBID")
         if len(arg) != 4:
             print("[FError]: Cannot continue. PDBID not valid.")
             raise ValueError
-        func(pp, arg)
+        func(ppx, arg)
         return wrapper
+
+
 
 
 class Protein:
     """
     """
-    @ErrorCheck
+    #@ErrorCheck
     def __init__(self, Pdb) -> None:
         self.pdbid = Pdb
-        self.url = "https://files.rcsb.org/view/{self.pdbid}.pdb"
+        self.url = f"https://files.rcsb.org/view/{self.pdbid}.pdb"
 
 
-p = Protein(sys.argv[-1])
-print(p.url)
+# write a argaprse to get pdbid and use it to create a protein object and print the url
+if __name__ == "__main__":
+    try:
+        pp = Protein(args.PDBID)
+    except ValueError:
+        sys.exit(1)
+    print(pp.url)
+
